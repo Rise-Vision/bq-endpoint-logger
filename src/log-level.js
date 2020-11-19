ef8140e14a5406853e4e2c7c7e46389730d4799f
Tyler Johnson <mail@tyler-johnson.ca>
2020-11-18 22:52:21 -0500

    Add log-level helper

---
+++ src/log-level.js
@@ -0,0 +1,39 @@
+import {retry} from "./retry-fn.js";
+
+const GCS_SERVICE_URL = "https://storage.googleapis.com/storage/v1/b/risevision-endpoint-loglevels/o/ENDPOINT_ID.json?alt=media";
+
+let refreshDate = localStorage.getItem("bq-endpoint-logger-fetchdate");
+let level = localStorage.getItem("bq-endpoint-logger-level");
+let serviceUrl = null;
+
+const THROTTLE_MAX_5_MIN = 300000;
+
+const recentlyFetched = ()=>new Date() - (refreshDate || 0) < THROTTLE_MAX_5_MIN;
+
+export const getLogLevel = ()=>{
+  if (level && recentlyFetched()) {return Promise.resolve(level);}
+
+  return retry(()=>fetch(serviceUrl))
+  .then(resp=>{
+    refreshDate = new Date();
+    return resp.json();
+  })
+  .then(json=>{
+    level = json.logLevel || "ERROR";
+    localStorage.setItem("bq-endpoint-logger-level", level);
+    localStorage.setItem("bq-endpoint-logger-fetchdate", refreshDate);
+    return level;
+  })
+  .catch(err=>{
+    console.error(err);
+    return Promise.resolve(level || "ERROR");
+  });
+};
+
+export const setUrl = id=>{
+  serviceUrl = GCS_SERVICE_URL.replace("ENDPOINT_ID", id);
+};
+
+export const reset = ()=>{
+  level = null;
+}
---
+++ src/log-level.test.js
@@ -0,0 +1,60 @@
+import {getLogLevel, reset} from "./log-level.js";
+
+describe.only("Log Level", ()=>{ // eslint-disable-line max-lines-per-function
+  before(()=>{
+    window.setTimeoutOrig = window.setTimeout;
+    window.fetchOrig = window.fetch;
+  });
+
+  afterEach(()=>{
+    window.setTimeout = window.setTimeoutOrig;
+    window.fetch = window.fetchOrig;
+    reset();
+  });
+
+  it("retrieves log level", ()=>{
+    window.fetch = ()=>{
+      return Promise.resolve({json(){return {logLevel: "INFO"}}});
+    };
+
+    return getLogLevel()
+    .then(level=>assert.equal(level, "INFO"));
+  });
+
+  it("doesn't fetch when recently retrieved", ()=>{
+    let secondAttempt = false;
+
+    window.fetch = ()=>{
+      return Promise.resolve({json(){return {logLevel: "INFO"}}});
+    };
+
+    return getLogLevel()
+    .then(()=>{
+      window.fetch = ()=>{
+        return Promise.reject(Error("test-error"));
+      };
+
+      window.setTimeout = (fn, ms)=>{
+        secondAttempt = true;
+        return window.setTimeoutOrig(fn, ms / 50);
+      };
+
+      return getLogLevel()
+    })
+    .then(level=>{
+      assert.equal(level, "INFO");
+      assert.equal(secondAttempt, false);
+    });
+  });
+
+  it("returns default ERROR level when level cannot be retrieved", ()=>{
+    window.setTimeout = (fn, ms)=>window.setTimeoutOrig(fn, ms / 50);
+
+    window.fetch = ()=>{
+      return Promise.reject(Error("test-error"));
+    };
+
+    return getLogLevel()
+    .then(level=>assert.equal(level, "ERROR"));
+  });
+});
