5d5c426b461ea016043809e8f92336d001c7e7d5
Tyler Johnson <mail@tyler-johnson.ca>
2020-11-20 17:00:44 -0500

    Handle 404s and ensure getlevel only fetches once
    If the library is reloaded it will use a cached value if available.
    
    If not available, it will fetch but only once. Subsequent calls return
    the same promise.

--- src/log-level.js
+++ src/log-level.js
@@ -1,12 +1,15 @@
 import {retry} from "./retry-fn.js";
 
 let refreshDate = localStorage.getItem("bq-endpoint-logger-fetchdate");
-let level = localStorage.getItem("bq-endpoint-logger-level");
+let levelCache = localStorage.getItem("bq-endpoint-logger-level");
 let serviceUrl = null;
 
 const GCS_SERVICE_URL = "https://storage.googleapis.com/storage/v1/b/risevision-endpoint-loglevels/o/ENDPOINT_ID.json?alt=media";
 
 const THROTTLE_MAX_5_MIN = 300000;
+
+// if level does not exist (most common case) gcs 404s with "No such object"
+const LOGLEVEL_NOT_PRESENT = "Loglevel file not present";
 
 const recentlyFetched = ()=>new Date() - (refreshDate || 0) < THROTTLE_MAX_5_MIN;
 
@@ -14,26 +17,40 @@ export const setEndpointId = id=>{
   serviceUrl = GCS_SERVICE_URL.replace("ENDPOINT_ID", id);
 };
 
-export const getLogLevel = ()=>{
-  if (level && recentlyFetched()) {return Promise.resolve(level);}
+let levelPromise = null;
 
-  return retry(()=>fetch(serviceUrl))
+export const getLogLevel = ()=>{
+  if (levelPromise) {return levelPromise;}
+  if (levelCache && recentlyFetched()) {return Promise.resolve(levelCache);}
+
+  return levelPromise = levelPromise || retry(()=>fetch(serviceUrl))
   .then(resp=>{
     refreshDate = new Date();
+
+    if (resp.status === 404) {
+      return Promise.reject(Error(LOGLEVEL_NOT_PRESENT));
+    }
+
     return resp.json();
   })
   .then(json=>{
-    level = json.logLevel || "ERROR";
+    const level = json.logLevel || "ERROR";
     localStorage.setItem("bq-endpoint-logger-level", level);
     localStorage.setItem("bq-endpoint-logger-fetchdate", refreshDate);
     return level;
   })
   .catch(err=>{
-    console.error(err);
-    return Promise.resolve(level || "ERROR");
+    const level = levelCache || "ERROR";
+
+    if (err.message !== LOGLEVEL_NOT_PRESENT) {
+      console.error(err);
+    }
+
+    return level;
   });
 };
 
-export const resetLevel = ()=>{
-  level = null;
+export const reset = ()=>{
+  levelPromise = null;
+  levelCache = null;
 }
